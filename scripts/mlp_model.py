a20fef687d567e9e8772c6cde618e7a2038fa034
joaoppagnan <joaopedroopagnan@gmail.com>
2021-05-28 21:55:01 -0300

    classe da mlp implementada e testada

--- scripts/mlp_model.py
+++ scripts/mlp_model.py
@@ -1,4 +1,9 @@
+import numpy as np
+import statistics
+import tensorflow as tf
+
 from tensorflow import keras
+from sklearn.metrics import mean_squared_error
 
 class ModeloMLP():
     
@@ -30,7 +35,7 @@ class ModeloMLP():
         self.modelo = None
         pass
         
-    def construir_mlp(batch_normalization='OFF', activation='selu',
+    def construir_mlp(self, batch_normalization='OFF', activation='selu',
                       init_mode='lecun_normal', n_neurons=30,
                       n_hidden_layers=1):
         """
@@ -80,10 +85,10 @@ class ModeloMLP():
         input_size = self._input_size
         
         # definindo o modelo
-        modelo = keras.Sequential(name=name)
+        modelo = keras.Sequential(name = name)
         
         # camada de entrada
-        modelo.add(keras.layers.Dense(input_size, input_dim=input_size, name="camada_de_entrada", activation = 'linear'))
+        modelo.add(keras.layers.Dense(input_size, input_dim=input_size, name="camada_de_entrada", activation='linear'))
         
         # camada de batch normalization
         if (batch_normalization == 'ON'):
@@ -91,9 +96,8 @@ class ModeloMLP():
         
         # adiciona n_hidden_layers camadas intermediárias
         for i in range(0, n_hidden_layers):
-            modelo.add(keras.layers.Dense(n_neurons, input_dim=input_size,
-                                          activation=activation, kernel_initializer=init_mode,
-                                          name="camada_intermediaria_"+str(i+1)))
+            modelo.add(keras.layers.Dense(n_neurons, activation=activation,
+                                          kernel_initializer=init_mode, name="camada_intermediaria_"+str(i+1)))
         
         # camada de saida
         modelo.add(keras.layers.Dense(1, activation='linear', name="camada_de_saida"))       
@@ -101,7 +105,7 @@ class ModeloMLP():
         self._modelo = modelo
         pass
     
-    def compilar(optimizer='Nadam', learning_rate=0.001, loss="mean_squared_error"):
+    def compilar(self, optimizer='Nadam', learning_rate=0.001, loss="mean_squared_error"):
         """
         Descrição:
         ----------
@@ -147,3 +151,270 @@ class ModeloMLP():
         
         self._modelo = modelo
         pass
+    
+    def visualizar(self):
+        """
+        Descrição:
+        ----------
+        Função para visualizar a rede neural com o summary do keras
+        
+        Parâmetros:
+        -----------
+        Nenhuma
+        
+        Retorna:
+        --------
+        Um sumário contendo os parâmetros da rede neural
+        """
+        
+        return self._modelo.summary()
+    
+    def fit(self, X_treino, X_val, y_treino, y_val, batch_size=10, early_stopping="ON", epochs=100):
+        """
+        Descrição:
+        ----------
+        Função para realizar o treinamento da rede
+        
+        Parâmetros:
+        -----------
+        X_treino: np.ndarray
+            Conjunto de entradas para o treinamento
+        X_val: np.ndarray
+            Conjunto de entradas para a validação
+        y_treino: np.ndarray
+            Conjunto de saidas para o treinamento
+        y_val: np.ndarray
+            Conjunto de saídas para a validação
+        batch_size: int
+            Tamanho do batch a ser utilizado durante o treinamento
+        early_stopping: str
+            Se deve "ON" ou não deve "OFF" utilizar early stopping
+        epochs: int
+            Número de épocas para o treinamento
+            
+        Retorna:
+        --------
+        Nada
+        """
+        
+        if not (type(X_treino) is np.ndarray):
+            raise TypeError("Os dados de entrada de treino devem ser um array do numpy!")  
+            
+        if not (type(X_val) is np.ndarray):
+            raise TypeError("Os dados de entrada de validação devem ser um array do numpy!")  
+            
+        if not (type(y_treino) is np.ndarray):
+            raise TypeError("Os dados de saída de treino devem ser um array do numpy!")  
+            
+        if not (type(y_val) is np.ndarray):
+            raise TypeError("Os dados de saída de validação devem ser um array do numpy!")  
+            
+        if not (type(batch_size) is int):
+            raise TypeError("O batch size deve ser um inteiro!")
+            
+        if not (type(early_stopping) is str):
+            raise TypeError("O parâmetro de early stopping deve ser uma string!")
+
+        if not ((early_stopping == 'ON') | (early_stopping == 'OFF')):
+            raise ValueError("A string do early stopping deve ser um ON ou OFF")            
+            
+        if not (type(epochs) is int):
+            raise TypeError("O número de épocas deve ser um int!")
+            
+        modelo = self._modelo
+        
+        if (early_stopping == 'ON'):
+            early_stopping_fit = keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True, monitor='val_loss')
+        else:
+            early_stopping_fit = None
+        
+        modelo.fit(X_treino, y_treino, 
+                   epochs=epochs, callbacks=early_stopping_fit,
+                   validation_data=(X_val, y_val), batch_size=batch_size,
+                   verbose=1)
+        
+        self._modelo = modelo
+        pass
+    
+    def predict(self, X_teste):
+        """
+        Descrição:
+        ----------
+        Função para predizer os próximos valores utilizando o conjunto de teste
+        
+        Parâmetros:
+        -----------
+        X_teste: np.ndarray
+            Conjunto de entradas para os dados de teste
+        
+        Retorna:
+        --------
+        As saídas previstas
+        """
+        
+        if not (type(X_teste) is np.ndarray):
+            raise TypeError("Os dados de entrada de teste devem ser um array do numpy!")
+            
+        modelo = self._modelo
+        
+        y_pred = modelo.predict(X_teste)
+        return y_pred
+    
+    def avaliar_modelo(self, X_treino, X_val, X_teste, y_treino, y_val, y_teste, n_repeticoes = 5, batch_size=10, early_stopping="ON", epochs=100):
+        """
+        Definição:
+        ----------
+        Função para treinar a rede e prever os dados n_repeticoes de vezes de forma a obter 
+        uma média e um desvio padrão para o erro quadrático médio
+        
+        Ela deve ser executada antes do fit! Ou seja, executar após o construir_mlp() e o compilar()
+        
+        Parâmetros:
+        -----------
+        X_treino: np.ndarray
+            Conjunto de entradas para o treinamento
+        X_val: np.ndarray
+            Conjunto de entradas para a validação
+        X_teste: np.ndarray
+            Conjunto de entradas para os dados de teste            
+        y_treino: np.ndarray
+            Conjunto de saidas para o treinamento
+        y_val: np.ndarray
+            Conjunto de saídas para a validação
+        y_teste: np.ndarray
+            Conjunto de saídas para o teste      
+        n_repeticoes: int
+            Número de repetições a serem feitas            
+        batch_size: int
+            Tamanho do batch a ser utilizado durante o treinamento
+        early_stopping: str
+            Se deve "ON" ou não deve "OFF" utilizar early stopping
+        epochs: int
+            Número de épocas para o treinamento
+
+        Retorna:
+        --------
+        A média e desvio padrão do erro quadrático médio para essa rede neural,
+        além de uma mensagem com essas informações
+        """
+
+        if not (type(n_repeticoes) is int):
+            raise TypeError("O número de repetições deve ser um inteiro!")
+        
+        if not (type(X_treino) is np.ndarray):
+            raise TypeError("Os dados de entrada de treino devem ser um array do numpy!")  
+            
+        if not (type(X_val) is np.ndarray):
+            raise TypeError("Os dados de entrada de validação devem ser um array do numpy!")  
+
+        if not (type(X_teste) is np.ndarray):
+            raise TypeError("Os dados de entrada de teste devem ser um array do numpy!")            
+            
+        if not (type(y_treino) is np.ndarray):
+            raise TypeError("Os dados de saída de treino devem ser um array do numpy!")  
+            
+        if not (type(y_val) is np.ndarray):
+            raise TypeError("Os dados de saída de validação devem ser um array do numpy!")  
+
+        if not (type(y_teste) is np.ndarray):
+            raise TypeError("Os dados de saída de teste devem ser um array do numpy!")              
+            
+        if not (type(batch_size) is int):
+            raise TypeError("O batch size deve ser um inteiro!")
+            
+        if not (type(early_stopping) is str):
+            raise TypeError("O parâmetro de early stopping deve ser uma string!")
+
+        if not ((early_stopping == 'ON') | (early_stopping == 'OFF')):
+            raise ValueError("A string do early stopping deve ser um ON ou OFF")            
+            
+        if not (type(epochs) is int):
+            raise TypeError("O número de épocas deve ser um int!")        
+        
+        conjunto_mse = []
+        
+        if (early_stopping == 'ON'):
+            early_stopping_fit = keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True, monitor='val_loss')
+        else:
+            early_stopping_fit = None
+        
+        for n in range(0, n_repeticoes):
+            modelo = self._modelo
+            
+            modelo.fit(X_treino, y_treino, 
+                   epochs=epochs, callbacks=early_stopping_fit,
+                   validation_data=(X_val, y_val), batch_size=batch_size,
+                   verbose=0)
+            
+            y_pred = modelo.predict(X_teste)
+            mse = mean_squared_error(y_teste, y_pred)
+            conjunto_mse.append(mse)
+        
+        mse_med = statistics.mean(conjunto_mse)
+        mse_dev = statistics.stdev(conjunto_mse)
+        
+        print("Média do erro quadrático médio: " + str(mse_med) + "\n")
+        print("Desvio padrão do erro quadrático médio: " + str(mse_dev) + "\n")
+        
+        return (mse_med, mse_dev)
+    
+    def salvar_modelo(self, nome_do_arquivo, h5="OFF"):
+        """
+        Definição:
+        ----------
+        Função para salvar o modelo num arquivo .h5 ou no padrão do Tensor Flow
+        
+        Parâmetros:
+        -----------
+        nome_do_arquivo: str
+            Nome do arquivo a ser salvo, podendo incluir o caminho
+        h5: str
+            Se deve "ON" ou não deve "OFF" salvar no formato .h5
+            
+        Retorna:
+        --------
+        Uma mensagem confirmando o salvamento
+        """
+        
+        if not (type(nome_do_arquivo) is str):
+            raise TypeError("O parâmetro de nome do arquivo deve ser uma string!")        
+
+        if not (type(h5) is str):
+            raise TypeError("O parâmetro de h5 deve ser uma string!")
+
+        if not ((h5 == 'ON') | (h5 == 'OFF')):
+            raise ValueError("A string do h5 deve ser um ON ou OFF")             
+        
+        modelo = self._modelo
+        
+        if (h5 == "ON"):
+            modelo.save(nome_do_arquivo, include_optimizer=True, save_format="h5")
+        else:
+            modelo.save(nome_do_arquivo, include_optimizer=True)
+        
+        return print("O modelo foi salvo!")
+    
+    def carregar_modelo(self, nome_do_arquivo):
+        """
+        Definição:
+        ----------
+        Função para carregar o modelo através de um arquivo .h5 ou padrão do Tensor Flow
+        
+        Parâmetros:
+        -----------
+        nome_do_arquivo: str
+            Nome do arquivo a ser salvo, podendo incluir o caminho
+            Deve incluir o formato!
+            
+        Retorna:
+        --------
+        Uma mensagem confirmando o carregamento
+        """
+        
+        if not (type(nome_do_arquivo) is str):
+            raise TypeError("O parâmetro de nome do arquivo deve ser uma string!")        
+        
+        modelo = keras.models.load_model(nome_do_arquivo)
+        self._modelo = modelo
+        
+        return print("O modelo foi carregado!")    
