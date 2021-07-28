76a65a4cd7f5f7e595653a6c72bdd15afd21b34c
joaoppagnan <joaopedroopagnan@gmail.com>
2021-07-27 21:10:28 -0300

    fiz as ultimas analises de K para os modelos

--- pibic2020/parameters/esn/esn_best_k.py
+++ pibic2020/parameters/esn/esn_best_k.py
@@ -1,4 +1,9 @@
 K_3best_henon_esn = [1, 2, 3]
 K_3best_logistic_esn = [2, 3, 4]
-K_3best_lorenz_esn = [5, 4, 6]
-k_3best_mackeyglass_esn = [1, 2, 3]
+K_3best_lorenz_esn = [2, 1, 3]
+K_3best_mackeyglass_esn = [1, 2, 3]
+
+K_best_henon_esn = [1]
+K_best_logistic_esn = [2]
+K_best_lorenz_esn = [2]
+K_best_mackeyglass_esn = [1]
--- pibic2020/parameters/mlp_basica/mlp_best_k.py
+++ pibic2020/parameters/mlp_basica/mlp_best_k.py
@@ -1,4 +1,9 @@
 K_3best_henon_mlp = [4, 8, 9]
 K_3best_logistic_mlp = [4, 3, 1]
 K_3best_lorenz_mlp = [5, 10, 8]
-k_3best_mackeyglass_mlp = [4, 7, 6]
+K_3best_mackeyglass_mlp = [4, 7, 6]
+
+K_best_henon_mlp = [4]
+K_best_logistic_mlp = [4]
+K_best_lorenz_mlp = [5]
+K_best_mackeyglass_mlp = [7]
